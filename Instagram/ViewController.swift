8a156a3328b29b1abec864b15349e749f35cb583
kirill titov <kirill.titov@outlook.com>
2022-09-27 22:26:19 +0300

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Титов Кирилл Иванович on 27.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = . red
+	}
+}
